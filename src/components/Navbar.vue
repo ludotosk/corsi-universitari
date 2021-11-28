610083ea756843362c92674a1f77983b5ed2a3eb
ludotosk <toscanoludovico@gmail.com>
2021-11-28 20:17:15 +0100

    fix navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,59 +1,104 @@
 <template>
   <div>
     <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-            <a href="/" class="navbar-item">
-                <img src="/img/icons/android-chrome-192x192.png" height="32" width="32" alt="logo corsiuniversitati.info" />
-            </a>
+      <div class="navbar-brand">
+        <a href="/" class="navbar-item">
+          <img
+            src="/img/icons/android-chrome-192x192.png"
+            height="32"
+            width="32"
+            alt="logo corsiuniversitati.info"
+          />
+        </a>
 
-            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
-        </div>
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbar"
+          v-on:click="isActive = !isActive"
+          v-bind:class="{ active: isActive }"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-        <div id="navbar" class="navbar-menu">
-            <div class="navbar-start has-text-weight-medium" style="flex-grow: 1; justify-content: center">
-                <a href="/" class="navbar-item">Tutti i corsi di laurea</a>
-                <a href="/corsi-di-laurea-online" class="navbar-item">Corsi online</a>
-                <a href="/corsi-di-laurea-in-inglese" class="navbar-item">In inglese</a>
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <p class="navbar-link">Città</p>
-                    <div class="navbar-dropdown">
-                        <a class="navbar-item" href="/corsi-di-laurea-bologna">Corsi di laurea Bologna
-                        </a>
-                        <a class="navbar-item" href="/corsi-di-laurea-firenze">Corsi di laurea Firenze
-                        </a>
-                        <a class="navbar-item" href="/corsi-di-laurea-milano">Corsi di laurea Milano
-                        </a>
-                        <a class="navbar-item" href="/corsi-di-laurea-padova">Corsi di laurea Padova
-                        </a>
-                        <a class="navbar-item" href="/corsi-di-laurea-roma">Corsi di laurea Roma</a>
-                        <a class="navbar-item" href="/corsi-di-laurea-torino">Corsi di laurea Torino
-                        </a>
-                        <a class="navbar-item" href="/corsi-di-laurea-bari">Corsi di laurea Bari</a>
-                        <a class="navbar-item" href="/corsi-di-laurea-pisa">Corsi di laurea Pisa</a>
-                        <a class="navbar-item" href="/corsi-di-laurea-napoli">Corsi di laurea Napoli
-                        </a>
-                        <a class="navbar-item" href="/corsi-di-laurea-palermo">Corsi di laurea Palermo
-                        </a>
-                    </div>
-                </div>
-                <a href="/lauree-triennali-elenco" class="navbar-item">Triennali</a>
-                <a href="/lista-corsi-di-laurea-senza-test" class="navbar-item">Corsi senza test</a>
-                <a href="https://www.corsiuniversitari.info/link" target="_blank" class="navbar-item">Test medicina</a>
+      <div id="navbar" class="navbar-menu" v-bind:class="{ active: isActive }">
+        <div
+          class="navbar-start has-text-weight-medium"
+          style="flex-grow: 1; justify-content: center"
+        >
+          <a href="/" class="navbar-item">Tutti i corsi di laurea</a>
+          <a href="/corsi-di-laurea-online" class="navbar-item">Corsi online</a>
+          <a href="/corsi-di-laurea-in-inglese" class="navbar-item"
+            >In inglese</a
+          >
+          <div class="navbar-item has-dropdown is-hoverable">
+            <p class="navbar-link">Città</p>
+            <div class="navbar-dropdown">
+              <a class="navbar-item" href="/corsi-di-laurea-bologna"
+                >Corsi di laurea Bologna
+              </a>
+              <a class="navbar-item" href="/corsi-di-laurea-firenze"
+                >Corsi di laurea Firenze
+              </a>
+              <a class="navbar-item" href="/corsi-di-laurea-milano"
+                >Corsi di laurea Milano
+              </a>
+              <a class="navbar-item" href="/corsi-di-laurea-padova"
+                >Corsi di laurea Padova
+              </a>
+              <a class="navbar-item" href="/corsi-di-laurea-roma"
+                >Corsi di laurea Roma</a
+              >
+              <a class="navbar-item" href="/corsi-di-laurea-torino"
+                >Corsi di laurea Torino
+              </a>
+              <a class="navbar-item" href="/corsi-di-laurea-bari"
+                >Corsi di laurea Bari</a
+              >
+              <a class="navbar-item" href="/corsi-di-laurea-pisa"
+                >Corsi di laurea Pisa</a
+              >
+              <a class="navbar-item" href="/corsi-di-laurea-napoli"
+                >Corsi di laurea Napoli
+              </a>
+              <a class="navbar-item" href="/corsi-di-laurea-palermo"
+                >Corsi di laurea Palermo
+              </a>
             </div>
-            <div class="navbar-end">
-                <a href="https://www.corsiuniversitari.info/link" class="navbar-item"><img src="/img/whatsapp-svgrepo-com.svg" alt="pagina gruppo whatsapp" height="40" width="40" /> Gruppo whatsapp</a>
-            </div>
+          </div>
+          <a href="/lauree-triennali-elenco" class="navbar-item">Triennali</a>
+          <a href="/lista-corsi-di-laurea-senza-test" class="navbar-item"
+            >Corsi senza test</a
+          >
+          <a
+            href="https://www.corsiuniversitari.info/link"
+            target="_blank"
+            class="navbar-item"
+            >Test medicina</a
+          >
         </div>
+        <div class="navbar-end">
+          <a href="https://www.corsiuniversitari.info/link" class="navbar-item"
+            ><img
+              src="/img/whatsapp-svgrepo-com.svg"
+              alt="pagina gruppo whatsapp"
+              height="40"
+              width="40"
+            />
+            Gruppo whatsapp</a
+          >
+        </div>
+      </div>
     </nav>
-
   </div>
 </template>
 
