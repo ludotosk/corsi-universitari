0a32bddfcc3a5a2b93c3c576e3c5184b6ac88554
ludotosk <toscanoludovico@gmail.com>
2021-11-28 16:25:42 +0100

    test multi page

---
+++ corsi-di-laurea-in-inglese/App.vue
@@ -0,0 +1,29 @@
+<script>
+import Nav from "../src/components/Navbar.vue";
+import Panel from "../src/components/Panellink.vue";
+import Footer from "../src/components/Footer.vue"
+import Corsi from "./corsi-di-laurea-in-inglese.vue"
+
+export default {
+  async beforeUpdate() {
+    try {
+      document.getElementById("navbar").setAttribute("class", `navbar-menu`);
+    } catch (e) {
+      //nulla
+    }
+  },
+  components: {
+    Nav,
+    Panel,
+    Footer,
+    Corsi
+  },
+};
+</script>
+
+<template>
+  <Nav />
+  <Corsi style="min-height: 100vh" />
+  <Panel />
+  <Footer />
+</template>
