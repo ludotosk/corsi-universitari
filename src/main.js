91dfff237740df16ea0fb1cdad0ccb629a21c049
ludotosk <toscanoludovico@gmail.com>
2021-11-28 11:16:13 +0100

    aggiunto presite

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
+//import { createApp } from 'vue'
+import { createApp as createClientApp, createSSRApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import SmartTable from 'vuejs-smart-table'
@@ -7,6 +8,7 @@ import VueGtag from "vue-gtag";
 import { createHead } from '@vueuse/head'
 
 const head = createHead()
+
 
 createApp(App).use(router).use(SmartTable).use(head).use(VueGtag, {
     property: {
