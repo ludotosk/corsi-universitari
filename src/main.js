c6d541b5f51589f13187551947b9cea39d0a7716
ludotosk <toscanoludovico@gmail.com>
2021-11-28 21:16:46 +0100

    fixato gtag

--- src/main.js
+++ src/main.js
@@ -4,11 +4,3 @@ import SmartTable from 'vuejs-smart-table';
 import './assets/style.scss';
 
 createApp(App).use(SmartTable).mount('#app')
-
-window.dataLayer = window.dataLayer || [];
-
-function gtag() {
-    window.dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'G-WSK0D83FEJ');
