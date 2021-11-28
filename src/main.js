26919dd6a3b84b97606a56bc814588edfecfa2cb
ludotosk <toscanoludovico@gmail.com>
2021-11-28 14:01:33 +0100

    tolto presite

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-//import { createApp } from 'vue'
-import { createApp as createClientApp, createSSRApp } from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import SmartTable from 'vuejs-smart-table'
