970c72e23f79920880c87398b05370a3fafbc0c8
ludotosk <toscanoludovico@gmail.com>
2021-11-29 15:59:58 +0100

    aggiunto gtag a tabella

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 const { resolve } = require('path')
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), VitePWA(
-
-    )],
+    plugins: [vue()],
     build: {
         rollupOptions: {
             input: {
