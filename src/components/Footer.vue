19d40b3966ca600bce262729485855153f432d09
ludotosk <toscanoludovico@gmail.com>
2022-03-31 14:34:10 +0200

    rimossi link fiverr

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,15 +4,17 @@
       <div class="content has-text-weight-medium has-text-centered">
         <p>
           Sviluppato a Morgex - Valle d'Aosta da Ludovico Toscano. Contatti:
-          <a href="mailto:corsiuniversitari@ludotosk.it" class="has-text-danger"
-            >corsiuniversitari@ludotosk.it</a
-          >, <a href="https://www.fiverr.com/share/XWbxaV" class="has-text-danger">link Fiverr</a>
+          <a
+            href="mailto:corsiuniversitari@ludotosk.it"
+            class="has-text-danger"
+          >corsiuniversitari@ludotosk.it</a>
           oppure
           <a href="/contattami" class="has-text-danger">usa questa pagina</a>.
           Per leggere l'informativa sui cookie
-          <a href="/cookie-policy" class="has-text-danger">
-            visita questa pagina</a
-          >.
+          <a
+            href="/cookie-policy"
+            class="has-text-danger"
+          >visita questa pagina</a>.
         </p>
       </div>
     </footer>
@@ -21,36 +23,39 @@
       <div class="modal-card">
         <header class="modal-card-head">
           <p class="modal-card-title">Unisciti ai nostri gruppi!</p>
-          <button class="delete" aria-label="close"  v-on:click="attiva()"></button>
+          <button class="delete" aria-label="close" v-on:click="attiva()"></button>
         </header>
         <section class="modal-card-body">
-          <h2 class="has-text-centered is-size-4 has-text-dark has-text-left">
-            Come possiamo aiutarti?
-          </h2>
+          <h2
+            class="has-text-centered is-size-4 has-text-dark has-text-left"
+          >Come possiamo aiutarti?</h2>
           <hr />
           <p>
             Abbiamo un
             <mark class="has-background-success">gruppo di orientamento</mark>,
             un
-            <mark class="has-background-success"
-              >gruppo dedicato ai test d'ingresso</mark
-            >
+            <mark class="has-background-success">gruppo dedicato ai test d'ingresso</mark>
             e in fine una
             <mark class="has-background-success">pagina Instagram per</mark> la
-            preparazione a <mark class="has-background-success">medicina</mark>!
+            preparazione a
+            <mark class="has-background-success">medicina</mark>!
           </p>
           <p>
-            Nel gruppo di <strong>orientamento</strong> troverai centania di
+            Nel gruppo di
+            <strong>orientamento</strong> troverai centania di
             persone che come te cercando di entrare nel corso giusto.
           </p>
           <p>
-            Nel gruppo per i <strong>test d'ingresso</strong> troverai persone
-            che come te si vogliono preparare ai <strong>tolc</strong> o al
+            Nel gruppo per i
+            <strong>test d'ingresso</strong> troverai persone
+            che come te si vogliono preparare ai
+            <strong>tolc</strong> o al
             <strong>test di medicina</strong>. Inoltre abbiamo un
             <strong>simulatore per i test</strong>.
           </p>
           <p>
-            Infine l'account <strong>Instagram per medicina</strong> conterrà
+            Infine l'account
+            <strong>Instagram per medicina</strong> conterrà
             informazioni utili per aiutarti a passare il test.
           </p>
         </section>
@@ -59,8 +64,7 @@
             href="https://www.corsiuniversitari.info/link"
             target="_blank"
             class="button is-primary"
-            >Link di accesso</a
-          >
+          >Link di accesso</a>
           <button class="button" v-on:click="attiva()">Chiudi</button>
         </footer>
       </div>
