19fb72801ea7d2ff2ba33ad2a789fe1614ab7cea
ludotosk <toscanoludovico@gmail.com>
2022-04-15 20:32:39 +0200

    test party town

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 const { resolve } = require('path')
 
+import path from 'path';
+import { partytownVite } from '@builder.io/partytown/utils';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        partytownVite({
+            dest: path.join(__dirname, 'dist', '~partytown'),
+        }),
+    ],
     build: {
         rollupOptions: {
             input: {
