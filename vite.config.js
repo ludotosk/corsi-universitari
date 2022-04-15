4106b4d63818aa4d51ffb5fbb7a184edd540ae9f
ludotosk <toscanoludovico@gmail.com>
2022-04-15 21:49:03 +0200

    tolto party town

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 const { resolve } = require('path')
 
-import path from 'path';
-import { partytownVite } from '@builder.io/partytown/utils';
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        vue(),
-        partytownVite({
-            dest: path.join(__dirname, 'dist', '~partytown'),
-        }),
+        vue()
     ],
     build: {
         rollupOptions: {
